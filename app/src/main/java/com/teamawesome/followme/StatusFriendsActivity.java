b73f50421448f97e70169b828b25ae80f8c15214
Alex <hicksa@southwestern.edu>
2015-04-15 17:55:19 -0500

    copied dialog and oncreate from friendsactivity to statusfriends activity

--- app/src/main/java/com/teamawesome/followme/StatusFriendsActivity.java
+++ app/src/main/java/com/teamawesome/followme/StatusFriendsActivity.java
@@ -1,17 +1,29 @@
 package com.teamawesome.followme;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.teamawesome.followme.adapters.FriendsAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 
@@ -26,12 +38,19 @@ public class StatusFriendsActivity extends ActionBarActivity {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     SectionsPagerAdapter mSectionsPagerAdapter;
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private StatusFragment.OnFragmentInteractionListener mListener;
     /**
      * The {@link ViewPager} that will host the section contents.
      */
     ViewPager mViewPager;
-
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,7 +65,65 @@ public class StatusFriendsActivity extends ActionBarActivity {
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
+
+
+        mListView = (ListView) findViewById(R.id.friends_listView);
+        List<String> list = new ArrayList<String>();
+        list.add("Android Jones");
+        list.add("Kevin Bacon");
+        list.add("Jonny Appleseed");
+        FriendsAdapter adapter = new FriendsAdapter(this, R.layout.listitem_friend, list);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                makeDialog().show();
+            }
+        });
     }
+
+
+    public Dialog makeDialog(){
+
+        final Context context = this;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.pick_follow)
+                .setItems(R.array.dialog_array, new DialogInterface.OnClickListener(){
+
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        Log.d("Onclick - ", "" + which);
+                        //to follow
+                        if (which == 0){
+
+                            Log.d("Onclick - Btn 1", ""+which);
+                            Intent i = new Intent(context, MapsActivity.class);
+                            startActivity(i);
+                            dialog.dismiss();
+                        }
+                        //to lead
+                        if (which == 1){
+
+                            Intent i = new Intent(context, MapsActivity.class);
+                            startActivity(i);
+                            dialog.dismiss();
+
+                        }
+                        //cancel
+                        if (which==2) dialog.dismiss();
+
+
+
+
+
+                    }
+                });
+        return builder.create();
+
+    }
+
 
 
     @Override
