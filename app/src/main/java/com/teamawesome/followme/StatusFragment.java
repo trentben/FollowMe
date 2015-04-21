65de3d11ebd9c650521dce36d6b9a2ccf636955d
trentben <trent.bennett27@gmail.com>
2015-04-21 11:17:38 -0500

    Status Fragment Improved

--- app/src/main/java/com/teamawesome/followme/StatusFragment.java
+++ app/src/main/java/com/teamawesome/followme/StatusFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 /**
@@ -18,16 +20,10 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class StatusFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private OnFragmentInteractionListener mListener;
+
+    private ImageView mBroadcastImage;
+    private Button mBroadcastBtn;
 
     /**
      * Use this factory method to create a new instance of
@@ -38,10 +34,7 @@ public class StatusFragment extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static StatusFragment newInstance() {
         StatusFragment fragment = new StatusFragment();
-        /*Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);*/
+
         return fragment;
     }
 
@@ -63,6 +56,8 @@ public class StatusFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_status, container, false);
+        mBroadcastImage = (ImageView) view.findViewById(R.id.broadcast_image);
+        mBroadcastBtn = (Button) view.findViewById(R.id.broadcast_button);
 
 
         return view;
