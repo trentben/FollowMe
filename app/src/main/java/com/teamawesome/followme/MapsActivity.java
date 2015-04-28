5d838e655594416e2d1f05ce3ad1669e6bdd96e4
Alex <hicksa@southwestern.edu>
2015-04-27 20:50:35 -0500

    Merge branch 'master' of https://github.com/trentben/FollowMe
    Conflicts:
    	app/src/main/res/values/strings.xml

--- app/src/main/java/com/teamawesome/followme/MapsActivity.java
+++ app/src/main/java/com/teamawesome/followme/MapsActivity.java
@@ -20,7 +20,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.teamawesome.followme.adapters.FriendsAdapter;
 import com.teamawesome.followme.util.Friend;
 
 import org.json.JSONArray;
